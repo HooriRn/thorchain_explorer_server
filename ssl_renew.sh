cc47149020b1fbd46ba7aab433be5e4118bce606
HooriRn <hoorirn@protonmail.com>
2023-02-26 02:00:10 +0330

    SLL renew

--- ssl_renew.sh
+++ ssl_renew.sh
@@ -4,4 +4,5 @@ DOCKER="/usr/bin/docker"
 
 cd /root/devs/explorer_server 
 $DOCKER compose run certbot renew --dry-run && $COMPOSE kill -s SIGHUP webserver
+$DOCKER compose up -d --force-recreate --no-deps webserver
 $DOCKER system prune -af
